3bb53ca21e006459054c6c13dfc22c21bee1936f
rakuom <poljaahub@gmail.com>
2024-02-25 13:55:11 +0300

    Customizing the UI with Background styling

--- InvestiChat.py
+++ InvestiChat.py
@@ -28,7 +28,7 @@ store = Chroma.from_documents(pages, embeddings, collection_name='annualreport')
 #Creating VectorStoreInfo and VectorStoreToolkit
 vectorstore_info = VectorStoreInfo(
     name = 'annual_report',
-    description = 'a company annual report as a pdf'
+    description = 'a company annual report as a pdf',
     vectorstore = store
 )# A vectorstoreinfo object is created that provides metadata about the pdf documents that are loaded. This includes
 # the name, description and the previously processed Chroma vector store.
@@ -45,7 +45,71 @@ agent_executor = create_vectorstore_agent(
 #Building the UI
 #Streamlit UI setup
 st.title('InvestiChat Assistant')
-prompt = st.text_input('Ask me anything about BAT')
-
+prompt = st.text_input('Ask me anything about Safaricom')
+#Customizing the UI with background styling
+def add_bg_from_url():
+    st.markdown(
+        f"""
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Mr+Dafoe&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Exo:wght@900$display=swap');
+        .stApp {{
+            background-image: linear-gradient(45deg, #FC466B, #3F5EFB);
+            background-attachement: fixed;
+            background-size: cover
+        }}
+        h1 {{text-align: center;
+    color: mistyrose;
+    font-family: Ariel, Helvetica, sans-serif;
+    opacity: 0.7;
+    font-size: 0.5rem;
+    margin-top: 0;
+    margin-bottom: 60px;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+    font-size: 50px;}}
+        h2 {{font-family: Arial, Helvetica, sans-serif;
+    margin: 0;
+    font-size: 2.5em;
+    margin-top: -0.6em;
+    color: white;
+    text-shadow: 0 0 0.05em #fff, 0 0 0.2em #fe05e1, 0 0 0.3em #fe05e1;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;}}
+        h3 {{font-family: Arial, Helvetica, sans-serif;
+    margin: 0;
+    font-size: 1.0em;
+    margin-top: -0.6em;
+    color: white;
+    text-shadow: 0 0 0.5em #fff, 0 0 0.2em #fe05e1, 0 0 0.3em #fe05e1;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;}}
+   /* Container style with flexbox layout to center the grid */
+           p {{color: #FFFFFF;
+    font-family: Ariel, Helvetica, sans-serif;
+    opacity: 0.7;
+    font-size: 28px;
+    margin-top: 0;
+    text-align: left;
+    margin-bottom: 60px;
+    text-shadow: 2px 2px 4px rgba(0, 0. 0.2);}}
+          [data-testid=stSidebar] {{background-color: rgba(255, 255, 255, 0.301);
+                                    border-left: 1px solid white;
+              border-top: 1px solid white;
+              backdrop-filter: blur(20px);
+              box-shadow: 20px 20px 40px -6px rgba(0,0,0,0.2);}}
+              transition: all 0.2s ease-in-out;
+              margin-left: auto;
+              margin-right: auto;
+              color: rgba(55, 58, 58);
+           font-family: Ariel, Helvetica, sans-serif;
+           opacity: 0.7;
+              text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+           </style>
+           """,
+           unsafe_allow_html=True
+    )
+add_bg_from_url()
 
 
